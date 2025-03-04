819bb4ed4d56a038e997cf4da706f6c526ecc54a
dhanraj0202 <dhanrajbeniwal34@gmail.com>
2025-03-04 11:12:02 +0530

    Greeting app with personalized message

--- src/main/java/com/example/GreetingApp/Controller/GreetingController.java
+++ src/main/java/com/example/GreetingApp/Controller/GreetingController.java
@@ -20,6 +20,13 @@ public class GreetingController {
     @GetMapping("/service")
     public String getGreetingServiceLayer() {
         return GreetingService.getGreetingMessage();
-}
+    }
+
+    @GetMapping("/personalized")
+    public String getGreeting(@RequestParam(required = false) String firstName,
+                              @RequestParam(required = false) String lastName) {
+        return greetingService.getGreetingMessage(firstName, lastName);
+    }
+
 
 }
